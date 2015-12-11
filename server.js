5009540bca702335e6f8f3d061c974fa2195065c
Jasonspd <jasoncluu@googlemail.com>
2015-12-11 16:11:36 +0000

    basic

---
+++ server.js
@@ -0,0 +1,38 @@
+var Hapi = require('hapi');
+
+var server = new Hapi.Server();
+server.connection({port:8080});
+
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: function (request, reply) {
+        reply('Hello, world!');
+    }
+});
+
+server.route({
+    method: 'GET',
+    path: '/{name}',
+    handler: function (request, reply) {
+        reply('Hello, ' + encodeURIComponent(request.params.name) + '!');
+    }
+});
+
+server.register(require('inert'), function (err) {
+    if (err) {
+        throw err;
+    }
+
+    server.route({
+        method: 'GET',
+        path: '/hello',
+        handler: function (request, reply) {
+            reply.file('./public/hello.html');
+        }
+    });
+});
+
+server.start(function() {
+	console.log('Server running at:', server.info.uri);
+});
